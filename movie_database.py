23c48c4a0bf7399ad33c3e0c8199e298dbe7c255
Remik Tkaczyk <r.t.kodilla@gmail.com>
2023-01-03 21:11:35 +0100

    Add class Series.

--- movie_database.py
+++ movie_database.py
@@ -18,3 +18,20 @@ class Movie:
     # Metoda '__str__' odpowiadająca za wygląd obiektu klasy 'Movie'
     def __str__(self):
         return f"{self.title} ({self.release_date})"    
+
+# Klasa 'Series' przechowująca informację na temat seriali znajdujących się w systemie i dziedzicząca z klasy 'Movie'        
+class Series(Movie):
+    # Domyślny konstruktor
+    def __init__(self, episode, season, *args, **kwargs):
+        # Odwołanie do klasy 'Movie' po której odziedzicza klasa 'Series'
+        super().__init__(*args, **kwargs)
+        # Powiązanie parametrów z atrybutami
+        self.episode = episode
+        self.season = season
+    # Metoda 'play' zwiększająca liczbę odtworzeń danego tytułu o 1    
+    def play(self, step=1):
+        return super().play(step)
+    # Metoda '__str__' odpowiadająca za wygląd obiektu klasy 'Movie'
+    def __str__(self):
+        return f"{self.title} S{self.episode}E{self.season}"
+#-----------------------------------------------------------------------------------------------------------------------        
